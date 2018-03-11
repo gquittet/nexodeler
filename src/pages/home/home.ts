eb72a31129704430838b0d1d7027f25ce25f80e7
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-03-11 21:48:00 +0100

    Created empty home page

---
+++ src/pages/home/home.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  pingSuccess: boolean;
+
+  constructor() {
+
+  }
+}
