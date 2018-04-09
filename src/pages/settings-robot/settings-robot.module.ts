b11e6b3cf8a06994b2b643b9e663fd409e910d0e
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-04-09 13:56:08 +0200

    Create robot settings page

--- src/pages/settings-robot/settings-robot.module.ts
+++ src/pages/settings-robot/settings-robot.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+
 import { SettingsRobotPage } from './settings-robot';
-import { VolumeControlModule } from '../../components/volume-control/volume-control.module';
-import { BatteryListItemModule } from '../../components/battery-list-item/battery-list-item.module';
-import { TemperatureListItemModule } from '../../components/temperature-list-item/temperature-list-item.module';
+
+import { ALAutonomousLifeListItemModule } from '../../components/list-items/al-autonomous-life-list-item/al-autonomous-life-list-item.module';
+import { ALMotionListItemModule } from '../../components/list-items/al-motion-list-item/al-motion-list-item.module';
+import { BatteryListItemModule } from '../../components/list-items/battery-list-item/battery-list-item.module';
+import { LanguagesAvailableListItemModule } from '../../components/list-items/languages-available-list-item/languages-available-list-item.module';
+import { NaoqiVersionListItemModule } from '../../components/list-items/naoqi-version-list-item/naoqi-version-list-item.module';
+import { RebootButtonModule } from '../../components/list-items/reboot-button/reboot-button.module';
+import { ShutdownButtonModule } from '../../components/list-items/shutdown-button/shutdown-button.module';
+import { TemperatureListItemModule } from '../../components/list-items/temperature-list-item/temperature-list-item.module';
+import { VolumeControlModule } from '../../components/list-items/volume-control/volume-control.module';
+
+import { ALModuleService } from '../../app/services/naoqi/almodule.service';
+import { TimezoneModule } from '../../components/list-items/timezone/timezone.module';
 
 @NgModule({
   declarations: [
@@ -11,12 +22,22 @@ import { TemperatureListItemModule } from '../../components/temperature-list-ite
   ],
   imports: [
     IonicPageModule.forChild(SettingsRobotPage),
+    ALAutonomousLifeListItemModule,
+    ALMotionListItemModule,
     BatteryListItemModule,
+    LanguagesAvailableListItemModule,
+    NaoqiVersionListItemModule,
+    RebootButtonModule,
+    ShutdownButtonModule,
     TemperatureListItemModule,
+    TimezoneModule,
     VolumeControlModule
   ],
   exports: [
     SettingsRobotPage
+  ],
+  providers: [
+    ALModuleService
   ]
 })
 export class SettingsRobotModule {}
--- src/pages/settings-robot/settings-robot.ts
+++ src/pages/settings-robot/settings-robot.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, IonicPage } from 'ionic-angular';
+import { NavController, NavParams, IonicPage, ViewController } from 'ionic-angular';
+import { IP } from '../../app/objects/IP';
+import { ALModuleService } from '../../app/services/naoqi/almodule.service';
+import { QiService } from '../../app/services/naoqi/qi.service';
 
 @IonicPage()
 @Component({
@@ -8,82 +11,14 @@ import { NavController, NavParams, IonicPage } from 'ionic-angular';
 })
 export class SettingsRobotPage {
 
-  // private batteryChart: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alModuleService: ALModuleService) { }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  ionViewCanEnter(): void {
+    const ip = new IP(this.navParams.get('ip').split('.'));
+    this.alModuleService.setIP(ip);
   }
 
-  // ionViewDidEnter() {
-  //   this.batteryChart = new Chart('batteryChart', {
-  //     type: 'doughnut',
-  //     data: {
-  //       datasets: [{
-  //         label: '# of Votes',
-  //         data: [75, 25],
-  //         backgroundColor: [
-  //           '#05c46b',
-  //           '#d2dae2'
-  //         ],
-  //         borderWidth: 1
-  //       }]
-  //     },
-  //     options: {
-  //       elements: {
-  //         center: {
-  //           text: '75%',
-  //           color: '#05c46b', // Default is #000000
-  //           fontStyle: 'Arial', // Default is Arial
-  //           sidePadding: 20 // Defualt is 20 (as a percentage)
-  //         }
-  //       },
-  //       tooltips: {
-  //         enabled: false
-  //       }
-  //     }
-  //   });
-
-  //   // Source : http://jsfiddle.net/nkzyx50o/
-  //   Chart.pluginService.register({
-  //     beforeDraw: function (chart) {
-  //       if (chart.config.options.elements.center) {
-  //         //Get ctx from string
-  //         const ctx = chart.chart.ctx;
-          
-  //         //Get options from the center object in options
-  //         const centerConfig = chart.config.options.elements.center;
-  //         const fontStyle = centerConfig.fontStyle || 'Arial';
-  //         const txt = centerConfig.text;
-  //         const color = centerConfig.color || '#000';
-  //         const sidePadding = centerConfig.sidePadding || 20;
-  //         const sidePaddingCalculated = (sidePadding/100) * (chart.innerRadius * 2)
-  //         //Start with a base font of 30px
-  //         ctx.font = "30px " + fontStyle;
-          
-  //         //Get the width of the string and also the width of the element minus 10 to give it 5px side padding
-  //         const stringWidth = ctx.measureText(txt).width;
-  //         const elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
-  
-  //         // Find out how much the font can grow in width.
-  //         const widthRatio = elementWidth / stringWidth;
-  //         const newFontSize = Math.floor(25 * widthRatio);
-  //         const elementHeight = (chart.innerRadius * 2);
-  
-  //         // Pick a new font size so it will not be larger than the height of label.
-  //         const fontSizeToUse = Math.min(newFontSize, elementHeight);
-  
-  //         //Set font settings to draw it correctly.
-  //         ctx.textAlign = 'center';
-  //         ctx.textBaseline = 'middle';
-  //         const centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
-  //         const centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
-  //         ctx.font = fontSizeToUse+"px " + fontStyle;
-  //         ctx.fillStyle = color;
-          
-  //         //Draw text in center
-  //         ctx.fillText(txt, centerX, centerY);
-  //       }
-  //     }
-  //   });
-  // }
-
+  ionViewWillLeave(): void {
+    QiService.disconnect();
+  }
 }
