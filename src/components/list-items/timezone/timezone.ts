25a58f5f1229642accec3b73db79d742bd1e099a
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-04-09 13:55:28 +0200

    Create timezone list item component

---
+++ src/components/list-items/timezone/timezone.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ALSystemService } from '../../../app/services/naoqi/alsystem.service';
+
+
+@Component({
+  selector: 'timezone',
+  templateUrl: 'timezone.html'
+})
+export class TimezoneComponent {
+
+  private timezoneInterval: number;
+
+  timezone: string;
+
+  constructor(private alSystem: ALSystemService) { }
+
+  ngOnInit(): void {
+    this.getTimezone();
+    this.timezoneInterval = setInterval(() => this.getTimezone(), 5000);
+  }
+
+  getTimezone(): void {
+    this.alSystem.getTimezone().then(timezone => this.timezone = timezone).catch(error => console.error("[ERROR][NAOQI][Call][ALSystemService] getTimezone: " + error));
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timezoneInterval);
+  }
+}
