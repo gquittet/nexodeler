3b75b2afa7b4710dc12b8048823b30d496f69047
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-04-09 13:58:30 +0200

    Correct the connection model to the robot.

--- src/app/services/naoqi/qi.service.ts
+++ src/app/services/naoqi/qi.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 
-import { IQiService } from './interfaces/IQiService';
 import { IP } from '../../objects/IP';
 
 declare var QiSession: any;
@@ -10,25 +9,25 @@ declare var QiSession: any;
  * @author Guillaume Quittet
  */
 @Injectable()
-export class QiService implements IQiService {
+export class QiService {
 
   /**
    * The session with the robot.
    */
-  private session;
+  private static session;
 
   /**
    * The module that are already loaded.
    */
-  private modulesLoaded = {};
+  private static modulesLoaded = {};
 
   /**
    * Return a table with the parameters of a function.
    * Source: https://stackoverflow.com/a/9924463
    * @param func A function
-   * @return {object} A table with the parameters of a function.
+   * @return {string[]} A table with the parameters of a function.
    */
-  private getParamsName(func: Function) {
+  private static getParamsName(func: Function): string[] {
     const STRIP_COMMENTS = /(\/\/.*$)|(\/\*[\s\S]*?\*\/)|(\s*=[^,\)]*(('(?:\\'|[^'\r\n])*')|("(?:\\"|[^"\r\n])*"))|(\s*=[^,\)]*))/mg;
     const ARGUMENT_NAMES = /([^\s,]+)/g;
     const fnStr = func.toString().replace(STRIP_COMMENTS, '');
@@ -38,14 +37,24 @@ export class QiService implements IQiService {
     return params;
   }
 
-  loadModule(moduleName: string): Promise<any> {
+  /**
+   * Load a module with its name.
+   * @param moduleName The name of a module.
+   * @return {Promise<any>} A promise that is ended when the module is loaded.
+   */
+  private static loadModule(moduleName: string): Promise<any> {
     if (!(moduleName in this.modulesLoaded)) {
       this.modulesLoaded[moduleName] = this.session.service(moduleName);
     }
     return this.modulesLoaded[moduleName];
   }
 
-  call(callback: Function): Promise<any> {
+  /**
+   * Call a NAOqi module and use it.
+   * @param callback A function to use a NAOqi module.
+   * @return {Promise<any>} The Promise
+   */
+  static call(callback: Function): Promise<any> {
     const modulesToBeLoaded = this.getParamsName(callback);
     let pendingModules = modulesToBeLoaded.length;
     const modulesLoaded = new Array(modulesToBeLoaded.length);
@@ -55,6 +64,7 @@ export class QiService implements IQiService {
           modulesLoaded[i] = module;
           pendingModules--;
           if (pendingModules === 0) {
+            console.log("[INFO][NAOQI][Call] " + modulesLoaded.toString());
             resolve(callback.apply(null, modulesLoaded));
           }
         }, () => {
@@ -65,22 +75,31 @@ export class QiService implements IQiService {
     });
   }
 
-  disconnect() {
+  /**
+   * Disconnect the current connection of the robot.
+   */
+  static disconnect(): void {
     if (this.session) {
       this.session.socket().removeAllListeners();
       this.session.socket().disconnect();
       this.session = null;
+      console.log("[INFO][NAOQI][Session] Disconnected");
     } else {
       throw "Cannot disconnect because there's no session active.";
     }
   }
 
-  connect(ip: IP) {
+  /**
+   * Connect to the robot or overwrite the current connection.
+   * @param ip The IP address of the robot.
+   */
+  static connect(ip: IP): void {
     if (!this.session) {
       this.session = new QiSession(ip.toString() + ':80');
       this.modulesLoaded = {};
       this.session.socket().on('connect', () => { });
       this.session.socket().on('disconnect', () => { });
+      console.log("[INFO][NAOQI][Session] Connected");
     } else {
       this.disconnect();
       this.connect(ip);
