64ef22158bf58c7bc4ef684335889825a01b3514
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-04-18 00:25:37 +0200

    Make basic fonctionnalities for list modules page

--- src/pages/list-modules/list-modules.ts
+++ src/pages/list-modules/list-modules.ts
@@ -5,6 +5,8 @@ import { ModulesService } from '../../app/services/modules/modules.service';
 import { Module } from '../../app/objects/Module';
 
 import { File } from '@ionic-native/file'
+import { Subscription } from 'rxjs/Subscription';
+import { TranslateService } from '@ngx-translate/core';
 
 
 @IonicPage()
@@ -20,45 +22,70 @@ export class ListModulesPage {
   searchTerm: string = '';
   searching: boolean;
 
-  private modules: Module[];
+  private subscription: Subscription;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private file: File, private modulesService: ModulesService) {
+  private modulesOriginal: Module[];
+  modules: Module[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private file: File, private modulesService: ModulesService, private translate: TranslateService) {
     this.searchControl = new FormControl();
   }
 
   ionViewDidLoad(): void {
+    // this.file.removeFile(this.file.dataDirectory, this.modulesService.FILE_NAME);
     this.file.checkFile(this.file.dataDirectory, this.modulesService.FILE_NAME).then(res => {
       if (res) {
         this.file.readAsText(this.file.dataDirectory, this.modulesService.FILE_NAME).then(data => {
-          this.modules = JSON.parse(data);
-          this.modulesService.update(this.modules);
+          this.modulesOriginal = JSON.parse(data);
+          this.modulesService.update(this.modulesOriginal);
         });
       }
     }, err => { });
   }
 
   ionViewDidEnter(): void {
+    this.subscription = this.modulesService.modules.subscribe((modules: Module[]) => this.modulesOriginal = modules);
     this.refreshModules();
   }
 
   private refreshModules(): void {
-    this.modulesService.modules.subscribe(
-      (modules: Module[]) => {
-        this.modules = modules;
-        if (this.section === 'favorites') {
-          this.modules = this.modules.filter((module: Module) => module.fav);
-        } else if (this.section === 'recents') {
-
-        }
-      },
-      (error) => console.error(error),
-      () => {
-
+    if (this.section === 'favorites') {
+      this.modules = this.modulesOriginal.filter((module: Module) => module.fav);
+    } else if (this.section === 'recents') {
+      this.modules = this.modulesOriginal.sort((a, b) => new Date(b.access).getTime() - new Date(a.access).getTime());
+    } else if (this.section === 'all') {
+      let nameA: string;
+      let nameB: string;
+      this.modules = this.modulesOriginal.sort((a, b) => {
+        this.translate.get('MODULES.NAMES.' + a.name).subscribe((res: string) => nameA = res);
+        this.translate.get('MODULES.NAMES.' + b.name).subscribe((res: string) => nameB = res);
+        return nameA.localeCompare(nameB);
       });
+    }
+  }
+
+  toggleFavorite(module: Module): void {
+    this.modulesOriginal = this.modulesOriginal.map((element: Module) => {
+      element === module ? element.fav = !element.fav : null;
+      return element;
+    });
+    this.modulesService.update(this.modulesOriginal);
+  }
+
+  openPage(module: Module): void {
+    this.modulesOriginal = this.modulesOriginal.map((element: Module) => {
+      element === module ? element.access = new Date() : null;
+      return element;
+    });
+    this.modulesService.update(this.modulesOriginal);
   }
 
   segmentChanged(): void {
     console.log(this.section);
     this.refreshModules();
+  }
+
+  ionViewWillLeave(): void {
+    this.subscription.unsubscribe();
   }
 }
