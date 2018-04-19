fac7fcb56bb670bedd6f823c3fd185c4a7396c67
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-04-19 21:38:45 +0200

    Edit module object

--- src/app/objects/Module.ts
+++ src/app/objects/Module.ts
@@ -1,12 +1,13 @@
 export interface Module {
+  readonly id: number;
   name: string;
   category: string;
-  fav: boolean;
   page: string;
+  fav: boolean;
   readonly creator: string;
   maintainer: string;
   version: string;
   created: Date;
-  update: Date;
-  access: Date;
+  updated: Date;
+  last_access: Date;
 }
