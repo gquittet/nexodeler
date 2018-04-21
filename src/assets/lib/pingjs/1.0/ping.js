8c3efd9cd5629e94956bbaebaa225361c5becc24
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-04-21 08:21:20 +0200

    Create ping robot function

--- src/assets/lib/pingjs/1.0/ping.js
+++ src/assets/lib/pingjs/1.0/ping.js
@@ -7,14 +7,14 @@
 var request_image = function (url) {
     return new Promise(function (resolve, reject) {
         var img = new Image();
-        img.onload = function () { resolve(img); };
-        img.onerror = function () { reject(url); };
+        img.onload = () => resolve(img);
+        img.onerror = () => reject(url);
         img.src = url + '?random-no-cache=' + Math.floor((1 + Math.random()) * 0x10000).toString(16);
     });
 };
 
 /**
- * Pings a url.
+ * Ping a url.
  * @param  {String} url
  * @returns {Promise} promise that resolves to a ping (ms, float).
  */
@@ -33,5 +33,27 @@ var ping = function (url) {
 
         // Set a timeout for max-pings, 3s.
         setTimeout(function () { reject(Error('Timeout')); }, 1000);
+    });
+};
+
+/**
+ * Ping a robot.
+ * @param {Robot} robot
+ */
+var pingRobot = function (robot) {
+    return new Promise((resolve, reject) => {
+        const start = (new Date()).getTime();
+        const response = () => {
+            var delta = ((new Date()).getTime() - start);
+
+            // HACK: Use a fudge factor to correct the ping for HTTP bulk.
+            delta /= 4;
+
+            resolve(robot);
+        };
+        request_image('http://' + robot.ip).then(response).catch(response);
+
+        // Set a timeout for max-pings, 3s.
+        setTimeout(() => reject(Error('Timeout')), 1000);
     });
 };
