26d6dffe553010c2927d2d02962fb9c54232f532
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-05-10 18:19:57 +0200

    Update list modules page

--- src/pages/list-modules/list-modules.module.ts
+++ src/pages/list-modules/list-modules.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { ListModulesPage } from './list-modules';
 import { TranslateModule } from '@ngx-translate/core';
+import { IonicPageModule } from 'ionic-angular';
 import { ModulesModule } from '../../app/services/modules/modules.module';
 import { RobotsModule } from '../../app/services/robots/robots.module';
+import { ListModulesPage } from './list-modules';
 
 @NgModule({
   declarations: [
