1c9aa4ca2a86aa8f3486d73090d79b6a3aef5ea3
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-05-19 20:43:47 +0200

    Change interface of add robot page

--- src/pages/add-robot/add-robot.ts
+++ src/pages/add-robot/add-robot.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { AlertController, IonicPage, LoadingController, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Subscription } from 'rxjs';
 import { IP } from '../../app/objects/IP';
 import { Robot } from '../../app/objects/Robot';
+import { Theme } from '../../app/objects/Theme';
 import { AlertLoading } from '../../app/objects/ionic/AlertLoading';
 import { ALSystemService } from '../../app/services/naoqi/alsystem.service';
 import { QiService } from '../../app/services/naoqi/qi.service';
 import { RobotsService } from '../../app/services/robots/robots.service';
+import { SettingsService } from '../../app/services/settings/settings.service';
 
 
 
@@ -24,21 +27,27 @@ export class AddRobotPage {
   robots: Robot[];
 
   private _errorText: string;
+  private _errorRobotAlreadyExit: string;
   private _errorUnableToGetRobotName: string;
   private _errorVerifyNetworkConnection: string;
   private _okText: string;
 
   private _loading: AlertLoading;
 
-  constructor(private _fb: FormBuilder, public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, private _alertCtrl: AlertController, loadingCtrl: LoadingController, private _robotsService: RobotsService, private _alSystemService: ALSystemService, translate: TranslateService) {
+  private _theme: Theme;
+  private _themeSubscription: Subscription;
+
+  constructor(private _fb: FormBuilder, public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController, private _alertCtrl: AlertController, loadingCtrl: LoadingController, private _robotsService: RobotsService, private _alSystemService: ALSystemService, translate: TranslateService, settingsService: SettingsService) {
     this.addForm = this._fb.group({
       'number1': ['', Validators.compose([Validators.required, Validators.min(0), Validators.max(255), Validators.minLength(1), Validators.maxLength(3)])],
       'number2': ['', Validators.compose([Validators.required, Validators.min(0), Validators.max(255), Validators.minLength(1), Validators.maxLength(3)])],
       'number3': ['', Validators.compose([Validators.required, Validators.min(0), Validators.max(255), Validators.minLength(1), Validators.maxLength(3)])],
       'number4': ['', Validators.compose([Validators.required, Validators.min(0), Validators.max(255), Validators.minLength(1), Validators.maxLength(3)])]
     });
-    this._loading = new AlertLoading(loadingCtrl, translate);
+    this._loading = new AlertLoading(loadingCtrl, translate, settingsService);
+    this._themeSubscription = settingsService.theme.subscribe((theme: Theme) => this._theme = theme);
     translate.get('ERROR.ERROR').subscribe((res: string) => this._errorText = res);
+    translate.get('ERROR.ROBOT_ALREADY_EXIST_IN_YOUR_LIST').subscribe((res: string) => this._errorRobotAlreadyExit = res);
     translate.get('ERROR.UNABLE_TO_GET_ROBOT_NAME').subscribe((res: string) => this._errorUnableToGetRobotName = res);
     translate.get('ERROR.VERIFY_NETWORK_CONNECTION').subscribe((res: string) => this._errorVerifyNetworkConnection = res);
     translate.get('OK').subscribe((res: string) => this._okText = res);
@@ -57,14 +66,30 @@ export class AddRobotPage {
         this._alertCtrl.create({
           title: this._errorText,
           subTitle: this._errorUnableToGetRobotName,
-          buttons: [this._okText]
+          buttons: [this._okText],
+          cssClass: this._theme.class
         }).present();
       }, 5000);
       QiService.connect(ip);
       this._alSystemService.getName().then(robotName => {
         const robot = new Robot(robotName, ip.toString());
-        this.robots.push(robot);
-        this._robotsService.next(this.robots);
+        let canAddTheRobot: boolean = true;
+        for (let r of this.robots) {
+          if (r.name === robot.name && r.ip === robot.ip) {
+            this._alertCtrl.create({
+              title: this._errorText,
+              subTitle: this._errorRobotAlreadyExit,
+              buttons: [this._okText],
+              cssClass: this._theme.class
+            }).present();
+            canAddTheRobot = false;
+            break;
+          }
+        }
+        if (canAddTheRobot) {
+          this.robots.push(robot);
+          this._robotsService.next(this.robots);
+        }
         clearTimeout(timer);
         this._loading.close();
         this.goBack();
@@ -74,7 +99,8 @@ export class AddRobotPage {
         this._alertCtrl.create({
           title: this._errorText,
           subTitle: this._errorUnableToGetRobotName,
-          buttons: [this._okText]
+          buttons: [this._okText],
+          cssClass: this._theme.class
         }).present();
       });
     }).catch(err => {
@@ -82,13 +108,18 @@ export class AddRobotPage {
       this._alertCtrl.create({
         title: this._errorText,
         subTitle: this._errorVerifyNetworkConnection,
-        buttons: [this._okText]
+        buttons: [this._okText],
+        cssClass: this._theme.class
       }).present();
     });
   }
 
   goBack(): void {
     this.navCtrl.remove(this.viewCtrl.index, 1);
+  }
+
+  ionViewWillLeave(): void {
+    this._themeSubscription.unsubscribe();
     QiService.disconnect();
   }
 }
