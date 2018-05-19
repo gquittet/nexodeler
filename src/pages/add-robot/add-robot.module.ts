c12b683f3b193c59b658891df18da27126704b43
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-05-19 15:06:53 +0200

    Remove useless modules

--- src/pages/add-robot/add-robot.module.ts
+++ src/pages/add-robot/add-robot.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ALSystemService } from '../../app/services/naoqi/alsystem.service';
-import { RobotsModule } from '../../app/services/robots/robots.module';
+import { RobotsService } from '../../app/services/robots/robots.service';
 import { AddRobotPage } from './add-robot';
 
 
@@ -13,14 +13,14 @@ import { AddRobotPage } from './add-robot';
   ],
   imports: [
     IonicPageModule.forChild(AddRobotPage),
-    RobotsModule,
     TranslateModule
   ],
   exports: [
     AddRobotPage
   ],
   providers: [
-    ALSystemService
+    ALSystemService,
+    RobotsService
   ]
 })
-export class AddRobotModule {}
+export class AddRobotModule { }
