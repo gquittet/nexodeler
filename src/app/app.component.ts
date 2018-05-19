4d586ea970dbd041fea6bfd0fea6b439861189c4
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-05-19 15:12:49 +0200

    Integrate the theme in the app component + fix sidenav bug (back button)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,74 @@
 import { Component } from '@angular/core';
-import { Config, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { Globalization } from '@ionic-native/globalization';
-import { TranslateService } from '@ngx-translate/core';
 import { AndroidPermissions } from '@ionic-native/android-permissions';
 import { Brightness } from '@ionic-native/brightness';
+import { Globalization } from '@ionic-native/globalization';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { TranslateService } from '@ngx-translate/core';
+import { App, Config, Platform } from 'ionic-angular';
+import { Theme } from './objects/Theme';
+import { SettingsService } from './services/settings/settings.service';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, translate: TranslateService, globalization: Globalization, androidPermissions: AndroidPermissions, brightness: Brightness, config: Config) {
-    platform.ready().then(() => {
-      statusBar.styleBlackTranslucent();
-      splashScreen.hide();
-      translate.setDefaultLang('en');
-      globalization.getPreferredLanguage().then(lang => {
-        translate.use(lang.value.split('-')[0]);
-        translate.get('UI.NAVBAR.BUTTONS.BACK').subscribe(res => {
-          config.set('ios', 'backButtonText', res);
-        });
-      }).catch(e => {
-        console.log('[ERROR][GLOBALIZATION][getLocale] ' + e);
-        translate.get('UI.NAVBAR.BUTTONS.BACK').subscribe(res => {
-          config.set('ios', 'backButtonText', res);
-        });
-      });
-      if (platform.is('ios') || platform.is('android')) {
-        brightness.setKeepScreenOn(true);
-        if (platform.is('android')) {
-          androidPermissions.requestPermissions([
-            androidPermissions.PERMISSION.ACCESS_NETWORK_STATE,
-            androidPermissions.PERMISSION.ACCESS_WIFI_STATE,
-            androidPermissions.PERMISSION.CHANGE_NETWORK_STATE,
-            androidPermissions.PERMISSION.CHANGE_WIFI_MULTICAST_STATE,
-            androidPermissions.PERMISSION.CHANGE_WIFI_STATE,
-            androidPermissions.PERMISSION.INTERNET,
-            androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE,
-            androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE
-          ]);
+  theme: Theme;
+
+  constructor(private _platform: Platform, private _statusBar: StatusBar, private _splashScreen: SplashScreen, private _app: App, private _translate: TranslateService, private _globalization: Globalization, private _androidPermissions: AndroidPermissions, private _brightness: Brightness, private _config: Config, settingsService: SettingsService) {
+    settingsService.theme.subscribe((theme: Theme) => this.theme = theme);
+    this.initializeApp();
+  }
+
+  private initializeApp(): void {
+    this._platform.ready().then(() => {
+      this._statusBar.styleBlackTranslucent();
+      this._splashScreen.hide();
+      // Fix sidemenu icon disappear in navbar when Android hardware back button pressed.
+      this._platform.registerBackButtonAction(() => {
+        const nav = this._app.getActiveNavs()[0];
+        if (nav.canGoBack()) {
+          nav.pop();
+        } else {
+          this._platform.exitApp();
         }
+      });
+      this.initializeTranslation();
+      if (this._platform.is('ios') || this._platform.is('android')) {
+        this._brightness.setKeepScreenOn(true);
+        if (this._platform.is('android'))
+          this.initializeAndroidPermissions();
       }
     });
+  }
+
+  private initializeTranslation(): void {
+    this._translate.setDefaultLang('en');
+    this._globalization.getPreferredLanguage().then(lang => {
+      this._translate.use(lang.value.split('-')[0]);
+      this._translate.get('UI.NAVBAR.BUTTONS.BACK').subscribe(res => {
+        this._config.set('ios', 'backButtonText', res);
+      });
+    }).catch(e => {
+      console.log('[ERROR][GLOBALIZATION][getLocale] ' + e);
+      this._translate.get('UI.NAVBAR.BUTTONS.BACK').subscribe(res => {
+        this._config.set('ios', 'backButtonText', res);
+      });
+    });
+  }
+
+  private initializeAndroidPermissions(): void {
+    this._androidPermissions.requestPermissions([
+      this._androidPermissions.PERMISSION.ACCESS_NETWORK_STATE,
+      this._androidPermissions.PERMISSION.ACCESS_WIFI_STATE,
+      this._androidPermissions.PERMISSION.CHANGE_NETWORK_STATE,
+      this._androidPermissions.PERMISSION.CHANGE_WIFI_MULTICAST_STATE,
+      this._androidPermissions.PERMISSION.CHANGE_WIFI_STATE,
+      this._androidPermissions.PERMISSION.INTERNET,
+      this._androidPermissions.PERMISSION.READ_EXTERNAL_STORAGE,
+      this._androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE
+    ]);
   }
 }
 
