f9b6f464c40d4662240f835a036e02c435d54a59
Guillaume Quittet <guillaume.quittet@gmail.com>
2018-05-20 23:33:00 +0200

    Replace showWhen by *ngIf

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { AlertController, IonicPage, MenuController, NavController } from 'ionic-angular';
+import { AlertController, IonicPage, MenuController, NavController, Platform } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -9,14 +9,17 @@ import { AlertController, IonicPage, MenuController, NavController } from 'ionic
 })
 export class HomePage {
 
+  isAndroid: boolean;
+
   private _errorText: string;
   private _noNetworkConnection: string;
   private _okText: string;
 
-  constructor(private _navCtrl: NavController, private _menuCtrl: MenuController, alertCtrl: AlertController, translate: TranslateService) {
+  constructor(platform: Platform, private _navCtrl: NavController, private _menuCtrl: MenuController, alertCtrl: AlertController, translate: TranslateService) {
     translate.get('UI.ALERT.CONTENT.LABEL.TO_USE_APP_ENABLE_NETWORK').subscribe((res: string) => this._noNetworkConnection = res);
     translate.get('ERROR.ERROR').subscribe((res: string) => this._errorText = res);
     translate.get('OK').subscribe((res: string) => this._okText = res);
+    this.isAndroid = platform.is('android');
   }
 
   openPage(page: string): void {
